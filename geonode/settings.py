0ace0db113e5c107ca40b23e9bf5b073160bc4df
Henning Bredel <h.bredel@52north.org>
2023-08-14 12:44:00 +0200

    Adjust example configuration

--- geonode/settings.py
+++ geonode/settings.py
@@ -4,31 +4,11 @@
 # https://docs.geonode.org/en/master/basic/settings/index.html#settings
 #
 
-# Load default settings
-from geonode.settings import *
-
 
 #DEBUG = "False"
 
-
-# Upload Settings
-DEFAULT_MAX_UPLOAD_SIZE = "1073741824"  # 1GB
-DEFAULT_MAX_PARALLEL_UPLOADS_PER_USER = 5
-
-
-# Adminstrator Settings
-ADMIN_EMAIL = "admin@localhost"
-ADMIN_PASSWORD = "admin"
-ADMIN_USERNAME = "admin"
-
-
-# Account Registration Settings
-ACCOUNT_OPEN_SIGNUP = "False"
-ACCOUNT_APPROVAL_REQUIRED = "False"
-ACCOUNT_AUTHENTICATION_METHOD = "username_email"
-ACCOUNT_CONFIRM_EMAIL_ON_GET = "False"
-ACCOUNT_EMAIL_REQUIRED = "True"
-ACCOUNT_EMAIL_VERIFICATION = "none"
+# sets defaults settings and from .env
+from geonode.settings import *
 
 
 # E-Mail Settings
@@ -37,6 +17,8 @@ EMAIL_HOST = "localhost"
 EMAIL_HOST_PASSWORD = ""
 EMAIL_HOST_USER = ""
 EMAIL_PORT = "25"
+# either SSL ..
 EMAIL_USE_SSL = "False"
+# .. or TLS
 EMAIL_USE_TLS = "False"
 DEFAULT_FROM_EMAIL = "GeoNode <no-reply@geonode.org>"
